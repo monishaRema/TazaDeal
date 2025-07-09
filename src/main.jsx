9133bd9ce89ab00649b393a842dbb601155b67dd
monisharema <monisharema97@gmail.com>
2025-07-09 03:32:14 +0300

    Project folder structure setup, firebase configaration, layout setup, header and footer, auth layout with signin and register

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { routes } from "./Routes/Routes.jsx";
+import AuthProvider from "./Contex/AuthProvider.jsx";
+import "./index.css";
+import "./app.css";
+import { HelmetProvider } from "@dr.pogodin/react-helmet";
+import { Slide, ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={routes} />
+      </AuthProvider>
+       <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
+    </HelmetProvider>
+  </StrictMode>
+);
